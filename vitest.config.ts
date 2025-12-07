b291c839b6be2ed176e64694b7a7b55c410bec7c
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-07 09:58:45 -0800

    Added standardized vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,9 @@ import {defineConfig} from "vitest/config";
 
 export default defineConfig({
   test: {
-    testTimeout: 60000,
-    hookTimeout: 60000,
+    include: ["test/**/*.test.ts"],
+    environment: "node",
+    globals: true,
+    isolate: true,
   },
 });
