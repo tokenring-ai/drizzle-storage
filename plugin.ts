ac30cbb822bfc53e0724d822b2769f8b429d6a9c
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:38 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,43 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {CheckpointPluginConfigSchema} from "@tokenring-ai/checkpoint";
+import AgentCheckpointService from "@tokenring-ai/checkpoint/AgentCheckpointService";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import {createMySQLStorage, mysqlStorageConfigSchema} from "./mysql/createMySQLStorage.js";
+import packageJSON from "./package.json" with {type: "json"};
+import {createPostgresStorage, postgresStorageConfigSchema} from "./postgres/createPostgresStorage.js";
+import {createSQLiteStorage, sqliteStorageConfigSchema} from "./sqlite/createSQLiteStorage.js";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    const config = app.getConfigSlice("checkpoint", CheckpointPluginConfigSchema);
+
+    if (config) {
+      app.services
+        .waitForItemByType(AgentCheckpointService, (checkpointService) => {
+          for (const name in config.providers) {
+            const provider = config.providers[name];
+            if (provider.type === "sqlite") {
+              checkpointService.registerProvider(
+                name,
+                createSQLiteStorage(sqliteStorageConfigSchema.parse(provider))
+              );
+            } else if (provider.type === "mysql") {
+              checkpointService.registerProvider(
+                name,
+                createMySQLStorage(mysqlStorageConfigSchema.parse(provider))
+              );
+            } else if (provider.type === "postgres") {
+              checkpointService.registerProvider(
+                name,
+                createPostgresStorage(postgresStorageConfigSchema.parse(provider))
+              )
+            }
+          }
+        });
+    }
+  },
+} as TokenRingPlugin;
